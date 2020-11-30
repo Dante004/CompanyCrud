95b0cbc93134d5629309c13b7b7ec4134d713ba7
Dante <bartosz.bk04@gmail.com>
2020-11-30 14:58:53 +0100

    Added DataContext

---
+++ CompanyCryd/CompanyCrud.Models/DataContext/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CompanyCrud.Models.DataContext
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) :
+            base(options)
+        {
+
+        }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ CompanyCryd/CompanyCrud.Models/DataContext/DataContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CompanyCrud.Models.DataContext
+{
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+            optionsBuilder
+                .UseSqlServer("Server=(localdb)\\MSSQLLocalDb;Database=CompanyCrud;Trusted_Connection=True;");
+
+            return new DataContext(optionsBuilder.Options);
+        }
+    }
+}
