439c4d4cc3ba5cb5c30eef56559a0425cd90c700
Dante <bartosz.bk04@gmail.com>
2020-11-30 14:13:05 +0100

    Added models

---
+++ CompanyCryd/CompanyCrud.Models/Company.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyCrud.Models
+{
+    public class Company
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public int Establishment { get; set; }
+
+    }
+}
---
+++ CompanyCryd/CompanyCrud.Models/Employees.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CompanyCrud.Models
+{
+    public enum JobTitle
+    {
+        Administrator,
+        Developer,
+        Architect,
+        Manager
+    }
+
+    public class Employees
+    {
+        public long ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public JobTitle JobTitle { get; set; }
+    }
+}
