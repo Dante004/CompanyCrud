abcbd7813577d0b08ae20788488a85909c97b1e1
Dante <bartosz.bk04@gmail.com>
2020-11-30 14:58:05 +0100

    Added relation in models

--- CompanyCryd/CompanyCrud.Models/Company.cs
+++ CompanyCryd/CompanyCrud.Models/Company.cs
@@ -1,10 +1,12 @@
-﻿namespace CompanyCrud.Models
+﻿using System.Collections.Generic;
+
+namespace CompanyCrud.Models
 {
     public class Company
     {
         public long ID { get; set; }
         public string Name { get; set; }
         public int Establishment { get; set; }
-
+        public ICollection<Employee> Employes { get; set; }
     }
 }
--- CompanyCryd/CompanyCrud.Models/Employee.cs
+++ CompanyCryd/CompanyCrud.Models/Employee.cs
@@ -10,12 +10,13 @@ namespace CompanyCrud.Models
         Manager
     }
 
-    public class Employees
+    public class Employee
     {
         public long ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
         public JobTitle JobTitle { get; set; }
+        public Company Company { get; set; }
     }
 }
