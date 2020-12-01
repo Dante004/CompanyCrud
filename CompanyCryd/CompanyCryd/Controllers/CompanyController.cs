4e8bdefb4d90136c49f54d30786499e010ad391c
Dante <bartosz.bk04@gmail.com>
2020-12-01 11:55:39 +0100

    Added Comapny Controller

---
+++ CompanyCryd/CompanyCryd/Controllers/CompanyController.cs
@@ -0,0 +1,49 @@
+﻿using CompanyCrud.Logic;
+using CompanyCrud.Logic.Interfaces;
+using CompanyCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CompanyCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ICompanyLogic _logic;
+
+        public CompanyController(ICompanyLogic logic)
+        {
+            _logic = logic;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Company company, CancellationToken token = default)
+        {
+            var result = await _logic.AddCompany(company, token);
+
+            if(!result.Success)
+            {
+                result.AddErrorToModelState(ModelState);
+                return BadRequest(ModelState);
+            }
+
+            return CreatedAtAction(nameof(Create), result.Value);
+        }
+
+        [HttpDelete("/{id}")]
+        public async Task<IActionResult> Delete(long id, CancellationToken token = default)
+        {
+            var result = await _logic.DeleteCompany(id, token);
+
+            if (!result.Success)
+            {
+                result.AddErrorToModelState(ModelState);
+                return BadRequest(ModelState);
+            }
+
+            return Accepted();
+        }
+    }
+}
