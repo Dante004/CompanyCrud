81a802a90308e87997de6816c53b84ab2becfc92
Dante <bartosz.bk04@gmail.com>
2020-12-02 11:03:33 +0100

    Added Mappers

---
+++ CompanyCryd/CompanyCryd/Mappers/CompanyProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CompanyCrud.Dto;
+using CompanyCrud.Models;
+
+namespace CompanyCrud.Mappers
+{
+    public class CompanyProfile : Profile
+    {
+        public CompanyProfile()
+        {
+            CreateMap<Company, CompanyDto>()
+                .ReverseMap();
+            CreateMap<Employee, EmployeeDto>()
+                .ReverseMap();
+        }
+    }
+}
