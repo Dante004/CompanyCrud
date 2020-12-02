4ce1397bc34943496d0ce7f468bbdc2a253d9307
Dante <bartosz.bk04@gmail.com>
2020-12-02 11:03:40 +0100

    Added Dtos

---
+++ CompanyCryd/CompanyCryd/Dto/CompanyDto.cs
@@ -0,0 +1,12 @@
+﻿using CompanyCrud.Models;
+using System.Collections.Generic;
+
+namespace CompanyCrud.Dto
+{
+    public class CompanyDto
+    {
+        public string Name { get; set; }
+        public int Establishment { get; set; }
+        public ICollection<EmployeeDto> Employes { get; set; }
+    }
+}
---
+++ CompanyCryd/CompanyCryd/Dto/EmployeeDto.cs
@@ -0,0 +1,13 @@
+﻿using CompanyCrud.Models;
+using System;
+
+namespace CompanyCrud.Dto
+{
+    public class EmployeeDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public JobTitle JobTitle { get; set; }
+    }
+}
